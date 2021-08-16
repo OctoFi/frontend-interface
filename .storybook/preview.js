a86236816d095616220c8d7a41e857d8f587e33f
nathanjessen <nathan.jessen@gmail.com>
2021-08-16 18:54:25 +0100

    Add translation and theme to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { MemoryRouter } from "react-router";
-// import Providers from "../src/Providers";
+import ThemeProvider from "../src/theme";
+import "../src/i18n";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import "../src/global.scss";
-
 export const decorators = [
-  (Story) => (
-    <MemoryRouter initialEntries={["/"]}>
-      {/* <Providers> */}
-      <Story />
-      {/* </Providers> */}
-    </MemoryRouter>
-  ),
+	(Story) => (
+		<MemoryRouter initialEntries={["/"]}>
+			<ThemeProvider>
+				<Story />
+			</ThemeProvider>
+		</MemoryRouter>
+	),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
